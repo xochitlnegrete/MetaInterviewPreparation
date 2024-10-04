d05bb3617bc2a7c418cdc56cafadb58d0e35433a
xochitlnegrete <93846995+xochitlnegrete@users.noreply.github.com>
2024-10-04 10:09:35 -0600

    Update and rename StringToInteger to StringToInteger.java
    Finished StringToInteger.java - Changed num as int to long

--- src/xochitl/interview/meta/exercises/leetcode/strings/StringToInteger.java
+++ src/xochitl/interview/meta/exercises/leetcode/strings/StringToInteger.java
@@ -1,40 +1,32 @@
 class Solution {
     public int myAtoi(String s) {
         s = s.trim();
-        int num = 0;
+        long num = 0;
         boolean positive = true;
         int cont = 0;
         boolean change = false;
 
         for(char c : s.toCharArray()) {
-          if(c == '-')  {
-              if(cont > 0)   return num;
-            positive = false;
-          }
-          /*else if(c =='+')   {
-              positive = true;
-          }*/
-          else{
             if(!Character.isDigit(c)) {
-                if(c != '+')
-                    break;
+                if(change)  break;
+                if(c == '+')    change = true;
                 else  if(c == '-')  { 
                     positive = false;
+                    change = true;
                 }
+                else    break;
             }
             else{
-                if(num * 10 + Integer.parseInt(String.valueOf(c)) < num)    {
-                    if(positive) return 2147483647;
-                    return -2147483648;
-                }
                 num = num * 10 + Integer.parseInt(String.valueOf(c));
+                if(num > Integer.MAX_VALUE)    {
+                    if(positive) return Integer.MAX_VALUE;
+                    return Integer.MIN_VALUE;
+                }
                 cont++;
+                change = true;
             }
           }
-        }
-        
         if(!positive) num *= -1;
-//2147483648
-        return num;
+        return (int) num;
     }
 }
